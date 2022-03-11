70501684b1286aa832568e4e22c3b79cee4f13ea
detailswes <details.wes@gmail.com>
2022-03-11 20:18:38 +0530

    added configuration page

--- src/views/Activity/Configuration/configurationStyles.ts
+++ src/views/Activity/Configuration/configurationStyles.ts
@@ -1,86 +1,81 @@
-import { blackColor, sectionSpacer } from "@cenera/common/styles/common-styles";
-import { selectStyles } from '@cenera/common/styles/select-styles';
-export const styles = () => ({
-  errorLabel: {
-    color: `#f64953`,
-    fontSize: `12px`,
-    fontWeight: `500`,
-    display: 'block',
-    marginTop: '-12px',
+export const configurationStyle = () => ({
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
-  bgContainer: {
-    position: "relative",
-    zIndex: 1,
+  title: {
+    marginBottom: 15,
   },
-
-  Container: {
-    "@media (min-width: 1280px)": {
-      maxWidth: "66.666667%",
-      flexBasis: "66.666667%",
-      margin: "auto",
-      padding: "0",
-    },
+  pos: {
+    marginBottom: 12,
   },
-
-  tableContainer: {
-    ...sectionSpacer,
+  icon: {
+    minWdth: "20px",
   },
-  table: {
-    minWidth: 700,
-    "& td": {
-      paddingTop: "10px",
-      paddingBottom: "10px",
-      color: "#484848",
-      "@media(max-width:767px)": {
-        whiteSpace: "nowrap",
-        padding: "10px",
-        fontSize: "10px",
-      },
-    },
-    "& th": {
-      "@media(max-width:767px)": {
-        whiteSpace: "nowrap",
-        padding: "10px 5px",
-        fontSize: "10px",
-      },
-    },
+  listIcon: {
+    minWidth: "30px",
   },
-
-  label: {
-    fontWeight: "bold",
-    color: blackColor,
-  },
-  customeTableRow: {
-    "& th": {
-      backgroundColor: "#F7F5F5",
-      color: blackColor,
-    },
-    "& td": {
-      backgroundColor: "#F7F5F5",
-      color: blackColor,
-      fontWeight: "500",
-      lineHeight: "1.5rem",
-    },
-  },
-
-  topTableRow: {
-    backgroundColor: "#0079BC",
-    textTransform: "uppercase",
-    "& td": {
-      backgroundColor: "#0079BC",
-      textTransform: "uppercase",
+  listItem: {
+    paddingLeft: 0,
+    paddingRight: 0,
+    "& button": {
+      backgroundColor: "#FF9800",
       color: "#ffffff",
-      fontWeight: "500",
+      padding: "7px 10px",
+      lineHeight: 1,
+      fontSize: "14px",
+      fontWeight: "400",
     },
   },
-  matched: {
-    fontWeight: 500,
-    color: "#0079BC",
+
+  listItemText: {
+    "& span": {
+      fontSize: "14px",
+      fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+      fontWeight: 300,
+    },
   },
-  bottomTableRow: {
-    "& td": {
+
+  cardActions: {
+    display: ["-webkit-box", "-ms-flexbox", "flex"],
+    borderTop: "1px solid #ececec",
+    marginTop: "-10px",
+    WebkitBoxOrient: "vertical",
+    WebkitBoxDirection: "normal",
+    msFlexDirection: "column",
+    flexDirection: "column",
+    WebkitBoxAlign: "stretch",
+    msFlexAlign: "stretch",
+    alignItems: "stretch",
+    "& button": {
+      color: "#FFF",
       border: "none",
+      cursor: "pointer",
+      margin: ".3125rem 1px !important",
+      padding: "12px 30px",
+      position: "relative",
+      fontSize: "12px",
+      minWidth: "auto",
+      boxShadow: "0 2px 2px 0 rgba(153, 153, 153, 0.14), 0 3px 1px -2px rgba(153, 153, 153, 0.2), 0 1px 5px 0 rgba(153, 153, 153, 0.12)",
+      minHeight: "auto",
+      textAlign: "center",
+      transition: "box-shadow 0.2s cubic-bezier(0.4, 0, 1, 1), background-color 0.2s cubic-bezier(0.4, 0, 0.2, 1)",
+      fontWeight: 400,
+      lineHeight: 1.42857143,
+      whiteSpace: "nowrap",
+      willChange: "box-shadow, transform",
+      touchAction: "manipulation",
+      borderRadius: "3px",
+      letterSpacing: "0",
+      textTransform: "uppercase",
+      verticalAlign: "middle",
+      backgroundColor: "rgb(0, 172, 193)",
+      "&:hover": {
+        color: "#FFF",
+        boxShadow: "0 12px 20px -10px rgba(0, 172, 193,.28), 0 4px 20px 0 rgba(0, 0, 0,.12), 0 7px 8px -5px rgba(0, 172, 193,.2)",
+        backgroundColor: "#00acc1",
+      },
     },
   },
-  ...selectStyles(),
 });
--- src/views/Activity/Configuration/index.tsx
+++ src/views/Activity/Configuration/index.tsx
@@ -1,162 +1,177 @@
 import React from "react";
-import { GridContainer } from "@cenera/components/Grid";
-import Grid from "@material-ui/core/Grid";
-import { Card, CardBody } from "@cenera/components/Card";
-import { makeStyles } from "@material-ui/core";
-import { styles } from "./configurationStyles";
-// import { Button } from "@cenera/components/Button/Button";
-// import Checkbox from "@material-ui/core/Checkbox";
-
-import {withStyles, Theme, createStyles} from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
 
-const StyledTableCell = withStyles((theme: Theme) =>
-  createStyles({
-    head: {
-      backgroundColor: "#0079BC",
-      color: theme.palette.common.white,
-      textTransform: "uppercase",
-    },
-    body: {
-      fontSize: 14,
-    },
+import { configurationStyle } from "./configurationStyles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import TextField from "@material-ui/core/TextField";
+import DeleteOutlineIcon from "@material-ui/icons/DeleteOutline";
+import Typography from "@material-ui/core/Typography";
 
-    row: {
-      fontSize: 0,
-    },
-  })
-)(TableCell);
-
-const StyledTableRow = withStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      backgroundColor: theme.palette.common.white,
-    },
-  })
-)(TableRow);
-
-const BodyTableCell = withStyles(() =>
-  createStyles({
-    root: {
-      color: "#767676",
-    },
-  })
-)(TableCell);
-
-function createData(
-  startime: string,
-  endtime: string,
-  // duration: string,
-  team: string,
-  location: string,
-  warderobe: string,
-  activity: string
-) {
-  // return { startime, endtime, duration, team, location, warderobe, activity };
-  return { startime, endtime, team, location, warderobe, activity };
-}
-
-const rows = [
-  createData(
-    "10:00am",
-    "05:00am",
-    // "2:00 hrs", for duration 
-    "Art Boxing Club",
-    "United Kingdom",
-    "Ingen",
-    "Training"
-  ),
-  createData(
-    "10:00am",
-    "05:00am",
-    // "2:00 hrs",
-    "Monaco",
-    "Spain",
-    "Ingen",
-    "Training"
-  ),
-  createData(
-    "10:00am",
-    "05:00am",
-    // "2:00 hrs",
-    "Real Soccer",
-    "Italy",
-    "Ingen",
-    "Training"
-  ),
-  createData(
-    "Cupcake",
-    "05:00am",
-    // "2:00 hrs",
-    "Oxigeno club",
-    "Germany",
-    "Ingen",
-    "Match"
-  ),
-];
-
-// function createDataExpand(
-//   awayfield: string,
-//   awaydata: string,
-//   warderobefield: string,
-//   warderobedata: string
-// ) {
-//   return { awayfield, awaydata, warderobefield, warderobedata };
-// }
-
-// const rowsexpand = [
-//   createDataExpand("Away Team:", "Dataserver", "Warderobe B", "Room 2"),
-//   createDataExpand("Referee:", "", "Warderobe C", "Room 3"),
-// ];
-
-const useStyles = makeStyles(styles as any);
+const useStyles = makeStyles(configurationStyle as any);
 
 const Configuration = () => {
   const classes = useStyles();
-  return (
-    <div className="parent">
-    <GridContainer>
-    </GridContainer>
-    <div className={classes.Container}>
-      <Card>
-        {/* <CardHeader><h4>Locations</h4></CardHeader> */}
-        <CardBody>
-          <Grid container>
-            <Grid item xs={12}>
-              <TableContainer component={Paper} className={classes.tableContainer}>
-                <Table className={classes.table} aria-label="customized table">
-                  <TableHead>
-                    <TableRow>
-                      <StyledTableCell colSpan={8}>Locations</StyledTableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row) => (
-                      <StyledTableRow key={row.startime}>
-                        <BodyTableCell scope="row">
-                          {row.location}
-                        </BodyTableCell>
-                      </StyledTableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </Grid>
-          </Grid>
-          {/* <div className="" style={{ textAlign: "center", paddingBottom: "40px" }}>
-            <Button color="danger" style={{maxWidth: "100%"}}>Delete Selected Activities</Button>
-          </div> */}
-        </CardBody>
-      </Card>
-    </div>
-  </div>
-  )
-}
 
-export {Configuration}
+  return (
+    <>
+      <Container>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6} lg={4}>
+            <Paper className={classes.paper}>
+              <Card className={classes.root} variant="outlined">
+                <CardContent>
+                  <Typography variant="h6" component="h3">
+                    Location
+                  </Typography>
+                  <List>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>New York</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>France</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>Jaipur</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>Ajmer</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                  </List>
+                </CardContent>
+                <CardActions className={classes.cardActions}>
+                  <TextField margin="dense" id="outlined-basic" label="Add New Location" variant="outlined" />
+                  <Button variant="contained" color="secondary">
+                    Add Location
+                  </Button>
+                </CardActions>
+              </Card>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={4}>
+            <Paper className={classes.paper}>
+              <Card className={classes.root} variant="outlined">
+                <CardContent>
+                  <Typography variant="h6" component="h3">
+                    Warderobe
+                  </Typography>
+                  <List>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>New York</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>France</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>Jaipur</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>Ajmer</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                  </List>
+                </CardContent>
+                <CardActions className={classes.cardActions}>
+                  <TextField margin="dense" id="outlined-basic" label="Add New Location" variant="outlined" />
+                  <Button variant="contained" color="secondary" style={{ backgroundColor: "#00ACC1" }}>
+                    Add Warderobe
+                  </Button>
+                </CardActions>
+              </Card>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={4}>
+            <Paper className={classes.paper}>
+              <Card className={classes.root} variant="outlined">
+                <CardContent>
+                  <Typography variant="h6" component="h3">
+                    Activity
+                  </Typography>
+                  <List>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>New York</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>France</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>Jaipur</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                    <ListItem className={classes.listItem}>
+                      <ListItemIcon className={classes.listIcon}>
+                        <LocationOnIcon />
+                      </ListItemIcon>
+                      <ListItemText className={classes.listItemText}>Ajmer</ListItemText>
+                      <DeleteOutlineIcon />
+                    </ListItem>
+                  </List>
+                </CardContent>
+                <CardActions className={classes.cardActions}>
+                  <TextField margin="dense" id="outlined-basic" label="Add New Location" variant="outlined" />
+                  <Button variant="contained" color="secondary">
+                    Add Activity
+                  </Button>
+                </CardActions>
+              </Card>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export { Configuration };
